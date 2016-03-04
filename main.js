3f62ee32a9a08ea4bcd461d5d2b16114826fe122
Geoffroy Lesage <gefthefrench@gmail.com>
2016-03-03 20:31:38 -0600

    Updated su timeslot format

--- main.js
+++ main.js
@@ -3,24 +3,25 @@
  */
 var timeslotsPerHour = 2;
 
-exports.timeslot_to_time = function(timeslot_val)
-{
-   var timeConv = timeslot_val / timeslotsPerHour;
-   var minutes = (timeConv - Math.floor(timeConv))*60;
-   minutes = Math.floor(minutes).toString();
-   if (minutes.length == 1) minutes = minutes + "0";
+exports.timeslot_to_time = function(timeslot_val) {
+    var timeConv = timeslot_val / timeslotsPerHour;
+    var minutes = (timeConv - Math.floor(timeConv)) * 60;
+    minutes = Math.floor(minutes).toString();
+    if (minutes.length == 1) minutes = minutes + "0";
 
-   var time = Math.floor(timeConv).toString() + ":" + minutes;
+    var hours = Math.floor(timeConv).toString();
+    if (hours.length == 1) hours = "0" + hours;
 
-   return time;
+    var time = hours + ":" + minutes + ":";
+
+    return time;
 }
 
-timeToTimeslot = function(time_val)
-{
+timeToTimeslot = function(time_val) {
     var hours = time_val.split(":")[0];
     var minutes = time_val.split(":")[1];
 
-    var timeslot = (parseInt(hours)*timeslotsPerHour) + Math.floor((parseInt(minutes)/60)*timeslotsPerHour);
+    var timeslot = (parseInt(hours) * timeslotsPerHour) + Math.floor((parseInt(minutes) / 60) * timeslotsPerHour);
 
     return timeslot;
 }
@@ -42,28 +43,25 @@ exports.order_schedule_delay = timeToTimeslot(orderScheduleDelay);
  * Order status
  */
 var orderStatus = {
-                "UNKNOWN": 0,
-                "CREATED": 1,
-                "PENDING_SCHEDULING_PICKUP": 2,
-                "PENDING_PICKUP": 3,
-                "ONGOING_PICKUP": 4,
-                "PICKED_UP": 5,
-                "PROCESSING": 6,
-                "PROCESSED": 7,
-                "PENDING_SCHEDULING_DELIVERY": 8,
-                "PENDING_DELIVERY": 9,
-                "ONGOING_DELIVERY": 10,
-                "DELIVERED": 11,
-                "COMPLETED": 12
-            };
+    "UNKNOWN": 0,
+    "CREATED": 1,
+    "PENDING_SCHEDULING_PICKUP": 2,
+    "PENDING_PICKUP": 3,
+    "ONGOING_PICKUP": 4,
+    "PICKED_UP": 5,
+    "PROCESSING": 6,
+    "PROCESSED": 7,
+    "PENDING_SCHEDULING_DELIVERY": 8,
+    "PENDING_DELIVERY": 9,
+    "ONGOING_DELIVERY": 10,
+    "DELIVERED": 11,
+    "COMPLETED": 12
+};
 
-exports.status_for_order = function(status_val)
-{
-   var status;
-    for (var key in orderStatus)
-    {
-        if(orderStatus.hasOwnProperty(key) && status_val == orderStatus[key])
-        {
+exports.status_for_order = function(status_val) {
+    var status;
+    for (var key in orderStatus) {
+        if (orderStatus.hasOwnProperty(key) && status_val == orderStatus[key]) {
             status = key;
         }
     }
